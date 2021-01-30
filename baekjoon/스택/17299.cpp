2aa93c71078bcc0609cb39ceafc682dd5b3ad197
b-chae <cbr5964@naver.com>
2021-01-30 15:51:20 +0900

    오등큰수

---
+++ baekjoon/스택/17299.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int A[1000000];
+int F[1000001];
+int ans[1000000];
+vector<int> stack;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    int i;
+    for (i = 0; i < N; i++)
+    {
+        cin >> A[i];
+        F[A[i]]++;
+    }
+
+    for (i = N - 1; i >= 0; i--)
+    {
+
+        while (!stack.empty() && F[stack.back()] <= F[A[i]])
+        {
+            stack.pop_back();
+        }
+
+        if (stack.empty())
+        {
+            ans[i] = -1;
+            stack.push_back(A[i]);
+        }
+        else
+        {
+            ans[i] = stack.back();
+            stack.push_back(A[i]);
+        }
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
