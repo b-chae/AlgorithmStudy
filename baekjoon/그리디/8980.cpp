3f7c41525a768aade6b7b509d0a02e5c231eb403
b-chae <cbr5964@naver.com>
2021-05-03 20:10:38 +0900

    Update 8980.cpp

--- baekjoon/그리디/8980.cpp
+++ baekjoon/그리디/8980.cpp
@@ -1,65 +1,53 @@
 #include <iostream>
-#include <queue>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-typedef struct node
-{
-    int end, cost;
-
-    bool operator<(const node &a) const
-    {
-        if (end == a.end)
-            return cost < a.cost;
-        return end > a.end;
+class Delivery {
+public:
+    int from;
+    int to;
+    int cnt;
+    Delivery(int from, int to, int cnt) {
+        this->from = from;
+        this->to = to;
+        this->cnt = cnt;
     }
-} node;
-
-int N, C, M;
-priority_queue<node> pq[2001];
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> N >> C >> M;
-
-    int x, y, z;
-
-    for (int i = 0; i < M; i++)
-    {
-        cin >> x >> y >> z;
-        pq[x].push({y, z});
+};
+ 
+bool cmp(Delivery a, Delivery b) {
+    if (a.to < b.to) return true;
+    else return false;
+}
+ 
+int main(void) {
+    int n, c, m;
+    cin >> n >> c >> m;
+    vector<Delivery> list;
+    vector<int> left(n + 1, c);
+    int box_cnt = 0;
+    for (int i = 0; i < m; i++) {
+        int from, to, cnt;
+        cin >> from >> to >> cnt;
+        list.push_back(Delivery(from, to, cnt));
     }
+    sort(list.begin(), list.end(), cmp);
+    
+    for (auto d : list) {
 
-    int ans = 0;
-    int now = 0;
-
-    priority_queue<node> queue;
-
-    for (int i = 1; i <= N; i++)
-    {
-        while (!queue.empty() && queue.top().end == i)
-        {
-            now -= queue.top().cost;
-            ans += queue.top().cost;
-            queue.pop();
+        int min = left[d.from];
+        for (int i = d.from + 1; i < d.to; i++) {
+            if (min > left[i]) min = left[i];
         }
 
-        while (!pq[i].empty() && now < C)
-        {
-            int e = pq[i].top().end;
-            int c = pq[i].top().cost;
-
-            int k = min(c, C - now);
-            now += k;
-            pq[i].pop();
-            queue.push({e, k});
-            cout << i << " " << e << " " << k << " " << ans << " " << now << "\n";
+        int cnt = d.cnt;
+        if (min < cnt) cnt = min;
+ 
+        box_cnt += cnt;
+ 
+        for (int i = d.from; i < d.to; i++) {
+            left[i] -= cnt;
         }
     }
-
-    cout << ans << "\n";
+    cout << box_cnt;
     return 0;
 }
